f0537211f26a2b2d65370270bd467d4aecbce08f
Dmitry Klimenko <dedooman@mail.ru>
2021-02-25 19:41:53 +0500

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,11 +12,13 @@ const fullPathTest = (file) => path.join(__dirname, '..', '__fixtures__', file);
 const readTestFile = (file) => fs.readFileSync(fullPathTest(file), 'utf-8');
 
 test('test JSON', () => {
+  expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'))).toEqual(readTestFile('resultStylish.txt'));
   expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'stylish')).toEqual(readTestFile('resultStylish.txt'));
   expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'plain')).toEqual(readTestFile('resultPlain.txt'));
   expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'json')).toEqual(readTestFile('resultJSON.txt'));
 });
 test('test YML', () => {
+  expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'))).toEqual(readTestFile('resultStylish.txt'));
   expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'stylish')).toEqual(readTestFile('resultStylish.txt'));
   expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'plain')).toEqual(readTestFile('resultPlain.txt'));
   expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'json')).toEqual(readTestFile('resultJSON.txt'));

b104a7dd8d9e227d47fdc0084aa78262c8c6e169
Dmitry Klimenko <dedooman@mail.ru>
2021-02-26 20:06:14 +0500

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,25 +1,26 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import fs from 'fs';
 
-import gendiff from '../index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const fullPathTest = (file) => path.join(__dirname, '..', '__fixtures__', file);
-const readTestFile = (file) => fs.readFileSync(fullPathTest(file), 'utf-8');
+const readTestFile = (file) => fs.readFileSync(file, 'utf-8');
 
-test('test JSON', () => {
-  expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'))).toEqual(readTestFile('resultStylish.txt'));
-  expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'stylish')).toEqual(readTestFile('resultStylish.txt'));
-  expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'plain')).toEqual(readTestFile('resultPlain.txt'));
-  expect(gendiff(fullPathTest('before.json'), fullPathTest('after.json'), 'json')).toEqual(readTestFile('resultJSON.txt'));
-});
-test('test YML', () => {
-  expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'))).toEqual(readTestFile('resultStylish.txt'));
-  expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'stylish')).toEqual(readTestFile('resultStylish.txt'));
-  expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'plain')).toEqual(readTestFile('resultPlain.txt'));
-  expect(gendiff(fullPathTest('before.yml'), fullPathTest('after.yml'), 'json')).toEqual(readTestFile('resultJSON.txt'));
+const elements = [['json', 'stylish'], ['json', 'plain'], ['json', 'json'], ['yml', 'stylish'], ['yml', 'plain'], ['yml', 'json']];
+
+describe('gendiff', () => {
+  test.each(elements)('gendiff formats', (element, format) => {
+    const getPathToTest1 = fullPathTest(`before.${element}`);
+    const getPathToTest2 = fullPathTest(`after.${element}`);
+
+    const getPathToResult = fullPathTest(`result_${format}.txt`);
+    const dataResult = readTestFile(getPathToResult);
+
+    expect(genDiff(getPathToTest1, getPathToTest2, format)).toEqual(dataResult);
+  });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -4,10 +4,10 @@ import path from 'path';
 
 const parseFile = (file) => {
   if (path.extname(file) === '.json') {
-    return JSON.parse(fs.readFileSync(file));
+    return JSON.parse(fs.readFileSync(file, 'utf-8'));
   }
   if (path.extname(file) === '.yml') {
-    return yaml.load(fs.readFileSync(file));
+    return yaml.load(fs.readFileSync(file, 'utf-8'));
   }
   return 'error';
 };

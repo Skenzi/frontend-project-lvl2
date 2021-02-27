5f68e650437ca1f17118af3e9f2ad78ce97f91a4
Dmitry Klimenko <dedooman@mail.ru>
2021-02-27 16:31:46 +0500

    update genTree.js

--- src/genTree.js
+++ src/genTree.js
@@ -6,39 +6,20 @@ const genTree = (fileData1, fileData2) => {
 
   const addElemenet = (key) => {
     if (!_.has(fileData1, key)) {
-      return {
-        key,
-        valueAfter: fileData2[key],
-        status: 'added',
-      };
+      return { key, valueAfter: fileData2[key], status: 'added' };
     }
     if (!_.has(fileData2, key)) {
-      return {
-        key,
-        valueBefore: fileData1[key],
-        status: 'deleted',
-      };
+      return { key, valueBefore: fileData1[key], status: 'deleted' };
     }
     if (_.isObject(fileData1[key]) && _.isObject(fileData2[key])) {
-      return {
-        key,
-        children: genTree(fileData1[key], fileData2[key]),
-        status: 'nested',
-      };
+      return { key, children: genTree(fileData1[key], fileData2[key]), status: 'nested' };
     }
     if (fileData1[key] !== fileData2[key]) {
       return {
-        key,
-        valueBefore: fileData1[key],
-        valueAfter: fileData2[key],
-        status: 'changed',
+        key, valueBefore: fileData1[key], valueAfter: fileData2[key], status: 'changed',
       };
     }
-    return {
-      key,
-      valueAfter: fileData2[key],
-      status: 'unchanged',
-    };
+    return { key, valueAfter: fileData2[key], status: 'unchanged' };
   };
   return sortedKeys.map(addElemenet);
 };
